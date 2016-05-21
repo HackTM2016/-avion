b5b316c4a0a6d4cc75cf8403c3ed36cd97795600
wststreet2 <wststreet2@yahoo.com>
2016-05-21 18:26:16 +0300

    Set plane position Comment code

--- ts/game.ts
+++ ts/game.ts
@@ -3,9 +3,15 @@ var globalGame;
 const PLANE_WIDTH: number = 160;
 const PLANE_HEIGHT: number = 127;
 
-
+// Used for scaling and positioning
 var originalTileSize = 32;
 var tileSize = 16;
+
+enum GamePlayerState {
+    Initial,
+    Alive,
+    Dead
+}
 
 class Game {
 
@@ -13,51 +19,39 @@ class Game {
     canvasLayer1: HTMLCanvasElement;
     contextLayer0: CanvasRenderingContext2D;
     contextLayer1: CanvasRenderingContext2D;
+
     bgImage: any;
     hoverGridImage: any;
-    airplane: any;
+    airplaneImage: any;
+
+    airplanePosition: any = { x: 0, y: 0 };
+    gamePlayerState: GamePlayerState = GamePlayerState.Initial;
 
     Init() {
         var game = this;
         var globalGame = this;
-
+        
+        //
         game.canvasLayer0 = <HTMLCanvasElement>document.getElementById('canvasLayer0');
         game.canvasLayer1 = <HTMLCanvasElement>document.getElementById('canvasLayer1');
         game.contextLayer0 = game.canvasLayer0.getContext('2d');
         game.contextLayer1 = game.canvasLayer1.getContext('2d');
 
         game.bgImage = new Image();
-        game.airplane = new Image();
+        game.airplaneImage = new Image();
         game.hoverGridImage = new Image();
-
-        game.bgImage.onload = function () {
-
-            for (var i = 0; i < game.canvasLayer0.width / tileSize; i++) {
-                for (var j = 0; j < game.canvasLayer0.height / tileSize; j++) {
-                    game.contextLayer0.drawImage(game.bgImage, 0, 0,
-                        originalTileSize, originalTileSize,
-                        tileSize * i, tileSize * j,
-                        tileSize, tileSize);
-                }
-            }
-
-            game.contextLayer1.drawImage(game.airplane, 0, 0, // image, offsetX, offsetY
-                PLANE_WIDTH, PLANE_HEIGHT, // width, height
-                0, 0,                // canvasOffsetX, canvasOffsetY
-                PLANE_WIDTH / 2, PLANE_HEIGHT / 2); // canvasWidth, canvasHeight
-
-
-        }
-
+        
+        game.gamePlayerState = GamePlayerState.Initial;
+        
         game.bgImage.src = 'img/dot.png';
-        game.airplane.src = 'img/avion.png';
+        game.airplaneImage.src = 'img/avion.png';
         game.hoverGridImage.src = 'img/dot-hover.png';
 
-        game.canvasLayer1.addEventListener('mousemove', function (evt) {
-            var gridPos = game.GetGridPos(game, { x: evt.clientX, y: evt.clientY });
 
-            game.contextLayer0.clearRect(0, 0, game.canvasLayer0.width, game.canvasLayer0.height);
+// Handlers
+        game.bgImage.onload = function () {
 
+            // Draw grid manually (needs to be done manually for scaling)
             for (var i = 0; i < game.canvasLayer0.width / tileSize; i++) {
                 for (var j = 0; j < game.canvasLayer0.height / tileSize; j++) {
                     game.contextLayer0.drawImage(game.bgImage, 0, 0,
@@ -66,7 +60,26 @@ class Game {
                         tileSize, tileSize);
                 }
             }
+        }
+        
+        game.canvasLayer0.addEventListener('mousemove', function (evt) {
+            var gridPos = game.GetGridPos(game, { x: evt.clientX, y: evt.clientY });
 
+            // Clear
+            game.contextLayer0.clearRect(0, 0, game.canvasLayer0.width, game.canvasLayer0.height);
+
+            // Draw grid again 
+            // TO DO: move to separate function
+            for (var i = 0; i < game.canvasLayer0.width / tileSize; i++) {
+                for (var j = 0; j < game.canvasLayer0.height / tileSize; j++) {
+                    game.contextLayer0.drawImage(game.bgImage, 0, 0,
+                        originalTileSize, originalTileSize,
+                        tileSize * i, tileSize * j,
+                        tileSize, tileSize);
+                }
+            }
+            
+            // Draw yellow (highlited) grid dot
             game.contextLayer0.drawImage(game.hoverGridImage, 0, 0,
                 originalTileSize, originalTileSize,
                 tileSize * gridPos.x, tileSize * gridPos.y,
@@ -74,12 +87,42 @@ class Game {
 
         }, false)
 
+        game.canvasLayer1.addEventListener('click', function (evt) {
+
+            var gridClick = game.GetGridPos(game, { x: evt.clientX, y: evt.clientY });
+            
+            if (game.gamePlayerState == GamePlayerState.Initial) {
+                // First click, set plane position
+                game.gamePlayerState = GamePlayerState.Alive;
+                
+                game.airplanePosition = gridClick;
+
+                // Clear, forgot why I added this
+                game.contextLayer1.clearRect(0, 0, game.canvasLayer1.width, game.canvasLayer1.height);
+
+                game.contextLayer1.drawImage(game.airplaneImage, 0, 0, // image, offsetX, offsetY
+                    PLANE_WIDTH, PLANE_HEIGHT, // width, height
+                    game.airplanePosition.x * tileSize, game.airplanePosition.y * tileSize,   // canvasOffsetX, canvasOffsetY
+                    PLANE_WIDTH / 2, PLANE_HEIGHT / 2); // canvasWidth, canvasHeight
+            }
+            else if(game.gamePlayerState == GamePlayerState.Alive) {
+                // Do "Shoot"
+            }
+            else {
+                // Player is dead, do nothing?
+            }
+            
+        }, false);
+
     }
 
     GetGridPos(game, mousePos) {
+        // Position relative to canvas top-left corner
         var canvasRect = game.canvasLayer0.getBoundingClientRect();
         mousePos.x -= canvasRect.left;
         mousePos.y -= canvasRect.top;
+        
+        // Convert to tileSize steps
         var gridPos = { x: Math.floor(mousePos.x / tileSize), y: Math.floor(mousePos.y / tileSize) };
 
         return gridPos;
