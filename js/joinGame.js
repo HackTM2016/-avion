4ca6420da1d2b02920d20fc853d2f9e4014cfb0f
Claudiu Balanean <claudiuxyz@yahoo.com>
2016-05-22 01:23:39 +0300

    Changes

--- js/joinGame.js
+++ js/joinGame.js
@@ -1,12 +1,9 @@
 /// <reference path="interfaces.ts"/>
 /// <reference path="gameutils.ts"/>
-
 var JoinGameForm = (function () {
     function JoinGameForm() {
     }
     JoinGameForm.init = function () {
-        //var joinGameForm = <HTMLElement>document.getElementById("gameJoinForm")
-        //joinGameForm.style.display = "block"
         SetVisibleDiv("gameJoinForm");
     };
     return JoinGameForm;
