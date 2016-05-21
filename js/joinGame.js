1908deb4bb631925896bd0a360a2c1635ba33e6e
Sinmax <sinmax_mv@yahoo.com>
2016-05-21 23:08:51 +0300

    add lib/js files

---
+++ js/joinGame.js
@@ -0,0 +1,11 @@
+/// <reference path="gameInfo.ts"/>
+var JoinGameForm = (function () {
+    function JoinGameForm() {
+    }
+    JoinGameForm.init = function () {
+        var joinGameForm = document.getElementById("gameJoinForm");
+        joinGameForm.style.display = "block";
+    };
+    return JoinGameForm;
+}());
+//# sourceMappingURL=joinGame.js.map
