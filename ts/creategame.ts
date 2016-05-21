804b5de34cdaefcdda49ffec0a3f70ee4f4091b9
Claudiu Balanean <claudiuxyz@yahoo.com>
2016-05-22 00:58:56 +0300

    Added global function to set visible divs

--- ts/creategame.ts
+++ ts/creategame.ts
@@ -1,4 +1,5 @@
 /// <reference path="interfaces.ts"/>
+/// <reference path="gameutils.ts"/>
 
 class GameInfoModel
 {
@@ -23,16 +24,22 @@ class GameCreationController
         var info = new Lobby
         var name = (<HTMLInputElement>document.getElementsByName("gameName").item(0)).value
         var planesPerPlayer = (<HTMLInputElement>document.getElementsByName("planesCount").item(0)).value
+        var maxPlayerCount = (<HTMLInputElement>document.getElementsByName("maxPlayerCount").item(0)).value
+        
+        SetVisibleDiv("canvasesForm")
     }
     
 }
+
 
 function GameCreationMain() {
     var creationForm = <HTMLElement>document.getElementById("gameCreationForm")
     var model = new GameInfoModel()
     var controller = new GameCreationController(model)
     var addGame = <HTMLButtonElement>document.getElementsByName("addGame").item(0)
-    creationForm.style.display = "block"
+    
+    SetVisibleDiv("gameCreationForm")
+    
     addGame.onclick = function() { controller.AddNewGame() }   
 }
 
