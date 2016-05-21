a7d30bbf28c0c90539bddd8ab7072db34b2831f6
artcorpse <inkriptor_scn@yahoo.com>
2016-05-21 16:50:29 +0300

    Unknown

--- ts/db.ts
+++ ts/db.ts
@@ -6,18 +6,18 @@ interface Room{
 class Reader {
     dataRef: Firebase;
     public main() {
-        this.dataRef = new Firebase('https://project-4810418174258671406.firebaseio.com/');
-        this.dataRef.child('Rooms').once("value", function(snapshot) {
-            var roomsData = snapshot.val();
-            var alertstring: string = "Init:";
-            if (!roomsData)
-                alertstring+="None";
-            else
-                roomsData.forEach(i => {
-                    alertstring+=i.Status.toString(); 
-                });
-            alert(alertstring.toString()); 
-        });
+//        this.dataRef = new Firebase('https://project-4810418174258671406.firebaseio.com/');
+//        this.dataRef.child('Rooms').once("value", function(snapshot) {
+//            var roomsData = snapshot.val();
+//            var alertstring: string = "Init:";
+//            if (!roomsData)
+//                alertstring+="None";
+//            else
+//                roomsData.forEach(i => {
+//                    alertstring+=i.Status.toString(); 
+//                });
+//            alert(alertstring.toString()); 
+//        });
     }
 }
 
